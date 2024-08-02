c4da9a044d9323867ec6da578b067403b2ee9348
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-08-02 13:07:20 +0200

    migration, fillable  users

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'avatar',
+        'is_admin',
+        'blocked_at',
         'email',
         'password',
     ];
