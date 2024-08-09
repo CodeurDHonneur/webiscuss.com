cd98393c4db0e7e7016d3ecede12d4803e9e6f4a
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-08-09 12:21:26 +0200

    création de ressource pour les users

--- routes/channels.php
+++ routes/channels.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Broadcast;
 use App\Models\User;
 
 Broadcast::channel('online', function (User $user) {
-    return $user;
+    return $user ? new UserResource($user) : null;
 });
